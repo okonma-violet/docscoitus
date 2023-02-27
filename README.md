da025204abf3654aac828e377d898bc696ecf477
Okonma <okonmaviolet@gmail.com>
2023-02-28 03:07:46 +0500

    multiple xlsxs

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # docscoitus
 Документачья свадьба
 ## run params
->go run . <strong>[template config file path]</strong> <strong>[docx template file path]</strong> <strong>[new docx file path]</strong> <strong>[xlsx file path]</strong>
+>go run . <strong>[docx template file path]</strong><strong>[new docx file path]</strong>  <strong>[first template config file path]</strong> <strong>[first xlsx file path]</strong> <strong>[second template config file path]</strong> <strong>[second xlsx file path]</strong>  <strong>third,...</strong> 
 
 test example run:
 >go run . testconfig1.txt testtemplate1.docx testresult1.docx test1.xlsx
-## template config file <em>(by example testconfig1.txt)</em>
+## template config file <em>(by example examplecfg1.txt)</em>
 <em>sheet not necessary, by default takes first (0) sheet</em>
+<em>tagnames MUST NOT be the same in various configs</em>
 >sheet 1\
 >tag1 C1\
->tag2 c10
+>tag2 c10\
+>sheet SOMETHING\
+>tag3 a1\
+>tag4 a2
 ## tags for replace in docx's text
 tag example:
 >[[\_yourtagnamehere\_]]
--- main.go
+++ main.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-	"errors"
 	"os"
 	"strconv"
-	"time"
 
 	"github.com/nguyenthenguyen/docx"
 	"github.com/okonma-violet/confdecoder"
-	"github.com/okonma-violet/logs/encode"
-	"github.com/okonma-violet/logs/logger"
 	"github.com/tealeg/xlsx"
 )
 
 // DOCX FILE TAG EXAMPLE: [[_tag1_]]
+// TAG NAMING [[_sheet_]] IS FORBIDDEN
 // TEMPLATE CONFIG's ROW EXAMPLE: tag1 B13
-// TEMPLATE CONFIG's SHEET SPECIFYING EXAMPLE (ZERO BASED): sheet 0
+//
+// TEMPLATE CONFIG's SHEET SPECIFYING EXAMPLE: "sheet 0 (ZERO BASED)" OR "sheet [sheetname]"
+// SHEET MUST BE SPECIFIED BEFORE ROWS THIS SHEET IS APPLIED ON
 // IF NO SHEET SPECIFIED - IT TAKES FIRST (0)
 
 const (
@@ -30,103 +29,98 @@ type replaceData struct {
 }
 
 func main() {
-	args := os.Args
-	if len(args) < 5 || args[1] == "--help" {
-		println("format: [template config file path] [docx template file path] [new docx file path] [xlsx file path]")
+	args := os.Args[1:]
+	if len(args) < 4 || args[0] == "--help" {
+		println("format: [docx template file path] [new docx file path] [template config file path] [xlsx file path] (...[template config file path] [xlsx file path])")
 		return
 	}
 
-	tfd, err := confdecoder.ParseFile(args[1])
-	if err != nil {
-		println("Parsing template config file err: " + err.Error())
-		return
-	}
-	df, err := docx.ReadDocxFile(args[2])
+	df, err := docx.ReadDocxFile(args[0])
 	if err != nil {
 		println("Reading docx file err: " + err.Error())
 		return
 	}
 	defer df.Close()
 
-	xf, err := xlsx.OpenFile(args[4])
-	if err != nil {
-		println("Reading xlsx file err: " + err.Error())
+	if (len(args))%2 > 0 {
+		println("Xlsx's args must be paired (template's config filepath + xlsx filepath) (at least one pair)")
 		return
 	}
 
 	dx := df.Editable()
+	println("AAA", len(args)-1, len(args)-1 < 3)
+	for k := len(args) - 1; k > 1; k -= 2 {
+		println("BBB", k)
+		xf, err := xlsx.OpenFile(args[k])
+		if err != nil {
+			println("Reading xlsx file err: " + err.Error())
+			return
+		}
+		if len(xf.Sheets) == 0 {
+			println("Reading xlsx file err: no sheets found in file") // яхз возможно ли это
+			return
+		}
 
-	flsh := logger.NewFlusher(encode.DebugLevel)
-	l := flsh.NewLogsContainer("docscoitus")
+		tfd, err := confdecoder.ParseFile(args[k-1])
+		if err != nil {
+			println("Parsing template config file err: " + err.Error())
+			return
+		}
 
-	tmpl_rows := tfd.Rows()
-	var sht *xlsx.Sheet
-	var replaces []replaceData
+		sht := xf.Sheets[0]
+		for j := 0; j < len(tfd.Rows); j++ {
+			if tfd.Rows[j].Key == "sheet" {
+				shti, err := strconv.Atoi(tfd.Rows[j].Value)
+				if err != nil {
+					var ok bool
+					if sht, ok = xf.Sheet[tfd.Rows[j].Value]; !ok {
+						println("Xlsx template's config err: sheet specified neither by num nor by existing sheetname (" + args[k-1] + ")")
+						return
+					}
+				} else {
+					if len(xf.Sheets) < (shti + 1) {
+						println("Xlsx templates  config'err: specified sheet num is bigger than num of sheets in readed xlsx: " + strconv.Itoa(shti) + " you want (zero based), " + strconv.Itoa(len(xf.Sheets)) + " xlsx has")
+						return
+					}
+					sht = xf.Sheets[shti]
+				}
+				continue
+			}
 
-	for i := 0; i < len(tmpl_rows); i++ {
-		if tmpl_rows[i].Name == "sheet" {
-			shti, err := strconv.Atoi(tmpl_rows[i].Value)
+			if tfd.Rows[j].Key == "" || tfd.Rows[j].Value == "" {
+				println("Xlsx template's config err: bad row - name or value is empty")
+				return
+			}
+			icol, irow, err := xlsx.GetCoordsFromCellIDString(tfd.Rows[j].Value)
 			if err != nil {
-				l.Error("Template", errors.New("sheet specified, but not by num, atoi err: "+err.Error()))
-				goto divorce
+				println("Xlsx template's config err: getting coords from row value err: " + err.Error())
+				return
 			}
-			if len(xf.Sheets) < (shti + 1) {
-				l.Error("Template", errors.New("specified sheet num is bigger than num of sheets in readed xlsx: "+strconv.Itoa(shti)+" you want (zero based), "+strconv.Itoa(len(xf.Sheets))+" xlsx has"))
-				goto divorce
+			tag := tagopen + tfd.Rows[j].Key + tagclose
+			if irow >= sht.MaxRow {
+				println("Xlsx template's config err: specified row num in coords is bigger than num of rows in sheet, " + strconv.Itoa(irow) + " you want (zero based), " + strconv.Itoa(sht.MaxRow) + " sheet has")
+				return
 			}
-			sht = xf.Sheets[shti]
-			tmpl_rows = tmpl_rows[:i+copy(tmpl_rows[i:], tmpl_rows[i+1:])]
+			if icol >= sht.MaxCol {
+				println("Xlsx template's config err: specified column num in coords is bigger than num of columns in sheet, " + strconv.Itoa(icol) + " you want (zero based), " + strconv.Itoa(sht.MaxCol) + " sheet has")
+				return
+			}
+			cell, err := sht.Cell(irow, icol)
+			if err != nil {
+				println("Xlsx template's config err: sheet.Cell() err: " + err.Error())
+				return
+			}
+			if err = dx.Replace(tag, cell.String(), -1); err != nil {
+				println("Replace err: replacing tag \"" + tag + "\" with \"" + cell.String() + "\" err: " + err.Error())
+				return
+			}
+			println("Replace: replaced tag \"" + tag + "\" with \"" + cell.String() + "\"")
 		}
+
 	}
 
-	l.Debug("Template", "rows readed: "+strconv.Itoa(len(tmpl_rows)))
-	replaces = make([]replaceData, len(tmpl_rows))
-	for i := 0; i < len(tmpl_rows); i++ {
-		if tmpl_rows[i].Name == "" || tmpl_rows[i].Value == "" {
-			l.Error("Template", errors.New("bad row - name or value is empty"))
-			goto divorce
-		}
-		icol, irow, err := xlsx.GetCoordsFromCellIDString(tmpl_rows[i].Value)
-		if err != nil {
-			l.Error("Template", errors.New("getting coords from row value err: "+err.Error()))
-			goto divorce
-		}
-		replaces[i].tag = tagopen + tmpl_rows[i].Name + tagclose
-		l.Debug("Template", "readed row: tag="+replaces[i].tag+", coords x="+strconv.Itoa(icol)+" y="+strconv.Itoa(irow)+" ("+tmpl_rows[i].Value+")")
-
-		if irow >= sht.MaxRow {
-			l.Error("Template", errors.New("specified row num in coords is bigger than num of rows in sheet, "+strconv.Itoa(irow)+" you want (zero based), "+strconv.Itoa(sht.MaxRow)+" sheet has"))
-			goto divorce
-		}
-		if icol >= sht.MaxCol {
-			l.Error("Template", errors.New("specified column num in coords is bigger than num of columns in sheet, "+strconv.Itoa(icol)+" you want (zero based), "+strconv.Itoa(sht.MaxCol)+" sheet has"))
-			goto divorce
-		}
-		cell, err := sht.Cell(irow, icol)
-		if err != nil {
-			l.Error("Template", errors.New("sheet.Cell() err: "+err.Error()))
-			goto divorce
-		}
-		replaces[i].value = cell.String()
-		l.Debug("Xlsx", "got value for tag "+replaces[i].tag+": "+cell.Value)
+	if err = dx.WriteToFile(args[1]); err != nil {
+		println("Writing result file err: " + err.Error())
 	}
-	l.Debug("Template", "all rows are readed, all data from xlsx is gotten")
-
-	for i := 0; i < len(replaces); i++ {
-		if err = dx.Replace(replaces[i].tag, replaces[i].value, -1); err != nil {
-			l.Error("Replace", errors.New("replacing tag \""+replaces[i].tag+"\" with \""+replaces[i].value+"\" err: "+err.Error()))
-			goto divorce
-		}
-		l.Debug("Replace", "replaced tag \""+replaces[i].tag+"\" with \""+replaces[i].value+"\"")
-	}
-	l.Debug("Replace", "all done")
-
-	if err = dx.WriteToFile(args[3]); err != nil {
-		l.Error("Writing result file", err)
-	}
-	l.Debug("Writing result file", "result written to "+args[3])
-
-divorce:
-	flsh.Close()
-	flsh.DoneWithTimeout(time.Second * 5)
+	println("Done! Result written to " + args[1])
 }
